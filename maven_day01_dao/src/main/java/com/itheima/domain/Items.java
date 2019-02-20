0cf55d93a750586e298044fe84ae7d8c6c06d611
zqlruo <1012086415@qq.com>
2019-02-20 21:03:16 +0800

    把本地工程添加到git

---
+++ maven_day01_dao/src/main/java/com/itheima/domain/Items.java
@@ -0,0 +1,17 @@
+package com.itheima.domain;
+
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Data
+@ToString
+public class Items {
+    private Integer id;
+    private String name;
+    private Float price;
+    private String pic;
+    private Date createtime;
+    private String detail;
+}
